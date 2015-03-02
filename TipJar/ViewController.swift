67fcf910cbec9a12b0e4197593fbcf902ce242e3
David Lewis <davidlewis@icloud.com>
2015-03-02 08:51:26 -0800

    Initial Commit

---
+++ TipJar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TipJar
+//
+//  Created by David Lewis on 2-3-15.
+//  Copyright (c) 2015 David Lewis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
