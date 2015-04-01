9ba6c41625858c3e62d15537882f3b8ed911c8fc
David Lewis <davidlewis@icloud.com>
2015-04-01 00:21:39 -0700

    Looks better now

--- TipJar/BluetoothUtility.swift
+++ TipJar/BluetoothUtility.swift
@@ -11,7 +11,8 @@ import CoreBluetooth
 
 
 class BluetoothUtility {
-    
+        // Role needs definition.
+        // Accepts an identity and spawns the appropriate Bluetooth Utility
 }
 
 let tipJarServicesUUID:CBUUID = CBUUID(string: "559B8DBE-C46C-46E2-9D68-6E2F2703E4C1")
@@ -19,115 +20,156 @@ let performerIdentityServiceUUID:CBUUID = CBUUID(string: "559B8DBE-C46C-46E2-9D6
 let spectatorIdentityServiceUUID:CBUUID = CBUUID(string: "559B8DBE-C46C-46E2-9D68-6E2F2703E4C3")
 
 // Has performance data to share
-class TJCBPerformer: CBPeripheralManager, CBCentralManagerDelegate {
+class TJBluetoothPerformerUtility: CBPeripheralManager, CBCentralManagerDelegate {
     var discoveredCentrals:NSArray?
     
-    // Discovered a nearby peripheral
+    /* Discovered a nearby peripheral device
+        1. Compare CBUUIDs
+        2. Other stuff
+        ...
+        ?. Add the peripheral to known peripherals
+    */
     func centralManager(central: CBCentralManager!,
         didDiscoverPeripheral peripheral: CBPeripheral!,
         advertisementData: [NSObject : AnyObject]!,
         RSSI: NSNumber!) {
             
     }
-    // Successfully connected to a peripheral
+    /* Successfully connected to a peripheral
+        - needs description
+    */
     func centralManager(central: CBCentralManager!, didConnectPeripheral peripheral: CBPeripheral!) {
         
     }
-    // Disconnected a peripheral
+    /* Disconnected a peripheral
+        - needs description
+    */
     func centralManager(central: CBCentralManager!,
         didDisconnectPeripheral peripheral: CBPeripheral!,
         error: NSError!) {
             
     }
-    // Failed to connect to peripheral
+    /* Failed to connect to peripheral
+        - needs description
+    */
     func centralManager(central: CBCentralManager!,
         didFailToConnectPeripheral peripheral: CBPeripheral!,
         error: NSError!) {
             
     }
-    // Invoked when the CentralManager retrieves a list of CONNECTED peripherals
+    /* Invoked when the CentralManager retrieves a list of CONNECTED peripherals
+    - needs description
+    */
     func centralManager(central: CBCentralManager!,
         didRetrieveConnectedPeripherals peripherals: [AnyObject]!) {
             
     }
-    // Invoked when the CentralManager retrieves a list of KNOWN peripherals
+    /* Invoked when the CentralManager retrieves a list of KNOWN peripherals
+    - needs description
+    */
     func centralManager(central: CBCentralManager!,
         didRetrievePeripherals peripherals: [AnyObject]!) {
             
     }
-    // Invoked when about to restore CentralManager to previous state
+    /* Invoked when about to restore CentralManager to previous state
+    - needs description
+    */
     func centralManager(central: CBCentralManager!,
         willRestoreState dict: [NSObject : AnyObject]!) {
             
     }
-    // Invoked when the central manager’s state is updated. (required)
+    /* Invoked when the central manager’s state is updated. (required)
+    - needs description
+    */
     func centralManagerDidUpdateState(central: CBCentralManager!) {
         
     }
 }
 
 // Requests all nearby peripherals performance data
-class TJCBSpectator: CBCentralManager, CBPeripheralDelegate {
+class TJBluetoothSpectatorUtility: CBCentralManager, CBPeripheralDelegate {
     var discoveredPeripherals:NSArray?
     
-    // Invoked when you discover the peripheral’s available services
+    /* Invoked when you discover the peripheral’s available services
+    - needs description
+    */
     func peripheral(peripheral: CBPeripheral!,
         didDiscoverServices error: NSError!) {
             
     }
-    // Invoked when you discover included services within a specified service
+    /* Invoked when you discover included services within a specified service
+    - needs description
+    */
     func peripheral(peripheral: CBPeripheral!,
         didDiscoverIncludedServicesForService service: CBService!,
         error: NSError!) {
             
     }
-    // Invoked when you discover the characteristics of a specified service
+    /* Invoked when you discover the characteristics of a specified service
+    - needs description
+    */
     func peripheral(peripheral: CBPeripheral!,
         didDiscoverCharacteristicsForService service: CBService!,
         error: NSError!) {
             
     }
-    // Invoked when you discover the descriptors of a specified characteristic
+    /* Invoked when you discover the descriptors of a specified characteristic
+    - needs description
+    */
     func peripheral(peripheral: CBPeripheral!,
         didDiscoverDescriptorsForCharacteristic characteristic: CBCharacteristic!,
         error: NSError!) {
             
     }
-    // Invoked when you retrieve a specified characteristic’s value, or when the peripheral device notifies your app that the characteristic’s value has changed
+    /* Invoked when you retrieve a specified characteristic’s value, or when the peripheral device notifies your app that the characteristic’s value has changed
+    - needs description
+    */
     func peripheral(peripheral: CBPeripheral!,
         didUpdateValueForCharacteristic characteristic: CBCharacteristic!,
         error: NSError!) {
             
     }
-    // Invoked when you retrieve a specified characteristic descriptor's value
+    /* Invoked when you retrieve a specified characteristic descriptor's value
+    - needs description
+    */
     func peripheral(peripheral: CBPeripheral!,
         didUpdateValueForDescriptor descriptor: CBDescriptor!,
         error: NSError!) {
             
     }
-    // Invoked when you write data to a characteristic’s value
+    /* Invoked when you write data to a characteristic’s value
+    - needs description
+    */
     func peripheral(peripheral: CBPeripheral!,
         didWriteValueForCharacteristic characteristic: CBCharacteristic!,
         error: NSError!) {
             
     }
-    // Invoked when you write data to a characteristic descriptor’s value
+    /* Invoked when you write data to a characteristic descriptor’s value
+    - needs description
+    */
     func peripheral(peripheral: CBPeripheral!,
         didWriteValueForDescriptor descriptor: CBDescriptor!,
         error: NSError!) {
             
     }
-    // Invoked when the peripheral receives a request to start or stop providing notifications for a specified characteristic’s value
+    /* Invoked when the peripheral receives a request to start or stop providing notifications for a specified characteristic’s value
+    - needs description
+    */
     func peripheral(peripheral: CBPeripheral!,
         didUpdateNotificationStateForCharacteristic characteristic: CBCharacteristic!,
         error: NSError!) {
             
     }
-    // Invoked when a peripheral’s name changes
+    /* Invoked when a peripheral’s name changes
+    - needs description
+    */
     func peripheralDidUpdateName(peripheral: CBPeripheral!) {
         
     }
-    // Invoked when a peripheral’s services have changed
+    /* Invoked when a peripheral’s services have changed
+    - needs description
+    */
     func peripheral(peripheral: CBPeripheral!,
         didModifyServices invalidatedServices: [AnyObject]!) {
             
